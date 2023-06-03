d9cf4852b7904b9a5bef98f91c66f97f97c422cc
Ruman-Islam <rumanislam48@gmail.com>
2023-06-04 02:10:50 +0600

    feat: winston
    custom log setup

---
+++ src/shared/logger.ts
@@ -0,0 +1,59 @@
+import { createLogger, format, transports } from 'winston'
+import DailyRotateFile from 'winston-daily-rotate-file'
+const { combine, timestamp, label, printf, prettyPrint } = format
+import path from 'path'
+import process from 'process'
+
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  const date = new Date(timestamp)
+  return `${date.toDateString()} [${label}] ${level}: ${message}`
+})
+
+const logger = createLogger({
+  level: 'info',
+  format: combine(
+    label({ label: 'UMS' }),
+    timestamp(),
+    myFormat,
+    prettyPrint()
+  ),
+  transports: [
+    new transports.Console(),
+    new DailyRotateFile({
+      filename: path.join(
+        process.cwd(),
+        'logs',
+        'winston',
+        'success',
+        'UMS-%DATE%-success.log'
+      ),
+      datePattern: 'YYYY-DD-MM-HH',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d',
+    }),
+  ],
+})
+
+const errorLogger = createLogger({
+  level: 'error',
+  format: combine(label({ label: 'UMS' }), timestamp(), myFormat),
+  transports: [
+    new transports.Console(),
+    new DailyRotateFile({
+      filename: path.join(
+        process.cwd(),
+        'logs',
+        'winston',
+        'error',
+        'UMS-%DATE%-error.log'
+      ),
+      datePattern: 'YYYY-DD-MM-HH',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d',
+    }),
+  ],
+})
+
+export { logger, errorLogger }
