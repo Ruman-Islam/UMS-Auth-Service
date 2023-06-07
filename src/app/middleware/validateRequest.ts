dddc843e7a06f317483eb18ff6370ed4b0d7fac9
Ruman-Islam <rumanislam48@gmail.com>
2023-06-07 22:57:08 +0600

    fix: User model Generic error message modified

--- src/app/middleware/validateRequest.ts
+++ src/app/middleware/validateRequest.ts
@@ -9,21 +9,24 @@ If the validation succeeds, the next function is called to proceed to the next m
 If the validation fails, the error is passed to the next function, which will handle the error.
 This middleware can be used to validate the request data against a specified Zod schema before processing the request further.
 */
-const validateRequest = /*  'Zod schema' ==> */
-  (schema: AnyZodObject) =>
-  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    // middleware function/handler
-    try {
-      await schema.parseAsync({
-        body: req.body, // Passing for validations if body & body's schema exists
-        query: req.query, // Passing for validations if query & query's schema exists
-        params: req.params, // Passing for validations if params & params's schema exists
-        cookies: req.cookies, // Passing for validations if cookies & cookies's schema exists
-      });
-      return next();
-    } catch (error) {
-      return next(error);
-    }
-  };
+const validateRequest =
+  /*  'Zod schema' ==> */
+
+
+    (schema: AnyZodObject) =>
+    async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+      // middleware function/handler
+      try {
+        await schema.parseAsync({
+          body: req.body, // Passing for validations if body & body's schema exists
+          query: req.query, // Passing for validations if query & query's schema exists
+          params: req.params, // Passing for validations if params & params's schema exists
+          cookies: req.cookies, // Passing for validations if cookies & cookies's schema exists
+        });
+        return next();
+      } catch (error) {
+        return next(error);
+      }
+    };
 
 export default validateRequest;
